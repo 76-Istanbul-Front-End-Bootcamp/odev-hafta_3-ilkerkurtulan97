3b7a9d34f3557d3858507abc9fd6f9add9587ece
ilkerkurtulan97 <ilker.kurtulan@hotmail.com>
2020-12-16 19:56:05 +0000

    Used css hovers instead of functions. Clear picture button fixed

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ class Cat extends Animal {
     this.legs = 4;
     this.actionText = "Meoow";
     this.actionSoundName = "meow";
+    this.animalUrl = "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
   }
 
   putInTheDocument(){
@@ -46,8 +47,6 @@ class Cat extends Animal {
     super.action();
   }
 
-
-  
 }
 
 class Monkey extends Animal {
@@ -56,6 +55,7 @@ class Monkey extends Animal {
     this.legs = 2;
     this.actionText = "Scream";
     this.actionSoundName = "scream";
+    this.animalUrl = "https://images.immediate.co.uk/production/volatile/sites/3/2020/01/18198683-low_res-baby-chimp-rescue-ad65871.jpg?quality=90&resize=768%2C574";
   }
 
   putInTheDocument(){
@@ -73,69 +73,29 @@ Mila.putInTheDocument();
 let Charlie = new Monkey("Charlie");
 Charlie.putInTheDocument();
 
-var firstDiv = document.getElementsByTagName("tr")[1];
-var secondDiv = document.getElementsByTagName("tr")[2];
+
 var firstElement = document.getElementsByTagName("td")[0];
 var secondElement = document.getElementsByTagName("td")[3];
-var imageCanvasMila = document.getElementById("myCanvasMila");
-var imageCanvasCharlie = document.getElementById("myCanvasCharlie");
 var clearPictureButton = document.getElementById("clearPictureButton");
-var milaLegs = document.getElementsByTagName("td")[1];
-var charlieLegs = document.getElementsByTagName("td")[4];
+var animalImage = document.getElementById("animalImage");
+var imageCanvas = document.getElementsByClassName("imageCanvas");
 
-
-
-var imgMila = document.createElement("img");
-imgMila.src = "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
-var imgCharlie = document.createElement("img");
-imgCharlie.src = "https://images.immediate.co.uk/production/volatile/sites/3/2020/01/18198683-low_res-baby-chimp-rescue-ad65871.jpg?quality=90&resize=768%2C574";
-
-
-
-
-function changeColorMila(){
-  firstDiv.style.backgroundColor = "green";
-  firstElement.style.color = "white";
-  milaLegs.style.color = "white";
-}
-
-function changeColorCharlie(){
-  secondDiv.style.backgroundColor = "green";
-  secondElement.style.color = "white";
-  charlieLegs.style.color = "white";
-}
-
-function milaDefault(){
-  firstDiv.style.backgroundColor = "white";
-  firstElement.style.color = "black";
-  milaLegs.style.color = "black";
-}
-
-function charlieDefault(){
-  secondDiv.style.backgroundColor = "white";
-  secondElement.style.color = "black";
-  charlieLegs.style.color = "black";
-}
 
 function showPictureMila(){
-  imageCanvasMila.appendChild(imgMila);
+  animalImage.src = this.animalUrl;
 }
 
 function showPictureCharlie(){
-  imageCanvasCharlie.appendChild(imgCharlie);
+  animalImage.src = this.animalUrl;
 }
 
 function clearPicture(){
-  imageCanvasMila.removeChild(imgMila);
-  imageCanvasCharlie.removeChild(imgCharlie);
+  var animalImage = document.getElementById("animalImage");
+  animalImage.src = "";
 }
 
 
-
-firstDiv.addEventListener("mouseover", changeColorMila);
-firstDiv.addEventListener("mouseout", milaDefault);
-secondElement.addEventListener("mouseover", changeColorCharlie);
-secondElement.addEventListener("mouseout", charlieDefault);
-firstElement.addEventListener("click", showPictureMila);
-secondElement.addEventListener("click", showPictureCharlie);
+firstElement.addEventListener("click", showPictureMila.bind(Mila));
+secondElement.addEventListener("click", showPictureCharlie.bind(Charlie));
 clearPictureButton.addEventListener("click", clearPicture);
+
